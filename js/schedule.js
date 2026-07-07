e969398a8327a4a729e15ed7eee329075c51a674
ILOVECHEESE22 <sofiabscc@gmail.com>
2026-07-06 21:31:16 -0400

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,1 +1,71 @@
+const schedule = {
 
+    lunes: [
+        "Ed. Física",
+        "Ed. Física",
+        "Matemática",
+        "BREAK",
+        "Artes Visuales",
+        "Artes Visuales",
+        "BREAK",
+        "Inglés",
+        "Inglés",
+        "ALMUERZO",
+        "Historia"
+    ],
+
+    martes: [
+        "Afectividad",
+        "Afectividad",
+        "Ciencias Naturales",
+        "BREAK",
+        "Matemática",
+        "Matemática",
+        "BREAK",
+        "Historia",
+        "Música",
+        "ALMUERZO",
+        "Lenguaje"
+    ],
+
+    miercoles: [
+        "Tecnología",
+        "Matemática",
+        "Matemática",
+        "BREAK",
+        "Lenguaje",
+        "Ed. Cívica",
+        "Ciencias Naturales",
+        "ALMUERZO",
+        "Ciencias Naturales"
+    ],
+
+    jueves: [
+        "Lenguaje",
+        "Lenguaje",
+        "Historia",
+        "BREAK",
+        "Inglés",
+        "Inglés",
+        "BREAK",
+        "Matemática",
+        "Música",
+        "ALMUERZO",
+        "Artes Visuales"
+    ],
+
+    viernes: [
+        "Historia",
+        "Tecnología",
+        "Lenguaje",
+        "BREAK",
+        "Educación Física",
+        "Educación Física",
+        "BREAK",
+        "Matemática",
+        "Orientación",
+        "ALMUERZO",
+        "Consejo de Curso"
+    ]
+
+};
