0f1b1d6d81bb7200941678d2320c8b9251185df9
ILOVECHEESE22 <sofiabscc@gmail.com>
2026-07-06 22:08:49 -0400

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,71 +1,107 @@
 const schedule = {
 
-    lunes: [
-        "Ed. Física",
-        "Ed. Física",
-        "Matemática",
-        "BREAK",
-        "Artes Visuales",
-        "Artes Visuales",
-        "BREAK",
-        "Inglés",
-        "Inglés",
-        "ALMUERZO",
-        "Historia"
-    ],
+lunes: [
 
-    martes: [
-        "Afectividad",
-        "Afectividad",
-        "Ciencias Naturales",
-        "BREAK",
-        "Matemática",
-        "Matemática",
-        "BREAK",
-        "Historia",
-        "Música",
-        "ALMUERZO",
-        "Lenguaje"
-    ],
+{start:"08:00",end:"08:45",type:"class",subject:"Ed. Física"},
+{start:"08:45",end:"09:30",type:"class",subject:"Ed. Física"},
+{start:"09:30",end:"10:15",type:"class",subject:"Matemática"},
 
-    miercoles: [
-        "Tecnología",
-        "Matemática",
-        "Matemática",
-        "BREAK",
-        "Lenguaje",
-        "Ed. Cívica",
-        "Ciencias Naturales",
-        "ALMUERZO",
-        "Ciencias Naturales"
-    ],
+{start:"10:15",end:"10:40",type:"break",id:"r1"},
 
-    jueves: [
-        "Lenguaje",
-        "Lenguaje",
-        "Historia",
-        "BREAK",
-        "Inglés",
-        "Inglés",
-        "BREAK",
-        "Matemática",
-        "Música",
-        "ALMUERZO",
-        "Artes Visuales"
-    ],
+{start:"10:40",end:"11:25",type:"class",subject:"Artes Visuales"},
+{start:"11:25",end:"12:10",type:"class",subject:"Artes Visuales"},
 
-    viernes: [
-        "Historia",
-        "Tecnología",
-        "Lenguaje",
-        "BREAK",
-        "Educación Física",
-        "Educación Física",
-        "BREAK",
-        "Matemática",
-        "Orientación",
-        "ALMUERZO",
-        "Consejo de Curso"
-    ]
+{start:"12:10",end:"12:30",type:"break",id:"r2"},
+
+{start:"12:30",end:"13:15",type:"class",subject:"Inglés"},
+{start:"13:15",end:"14:00",type:"class",subject:"Inglés"},
+
+{start:"14:00",end:"14:55",type:"lunch"},
+
+{start:"14:55",end:"15:40",type:"class",subject:"Historia"}
+
+],
+
+martes:[
+
+{start:"08:00",end:"08:45",type:"class",subject:"Afectividad"},
+{start:"08:45",end:"09:30",type:"class",subject:"Afectividad"},
+{start:"09:30",end:"10:15",type:"class",subject:"Ciencias Naturales"},
+
+{start:"10:15",end:"10:40",type:"break",id:"r1"},
+
+{start:"10:40",end:"11:25",type:"class",subject:"Matemática"},
+{start:"11:25",end:"12:10",type:"class",subject:"Matemática"},
+
+{start:"12:10",end:"12:30",type:"break",id:"r2"},
+
+{start:"12:30",end:"13:15",type:"class",subject:"Historia"},
+{start:"13:15",end:"14:00",type:"class",subject:"Música"},
+
+{start:"14:00",end:"14:55",type:"lunch"},
+
+{start:"14:55",end:"15:40",type:"class",subject:"Lenguaje"}
+
+],
+
+miercoles:[
+
+{start:"08:00",end:"08:45",type:"class",subject:"Tecnología"},
+{start:"08:45",end:"09:30",type:"class",subject:"Matemática"},
+{start:"09:30",end:"10:15",type:"class",subject:"Matemática"},
+
+{start:"10:15",end:"10:40",type:"break",id:"r1"},
+
+{start:"10:40",end:"11:25",type:"class",subject:"Lenguaje"},
+{start:"11:25",end:"12:10",type:"class",subject:"Ed. Cívica"},
+{start:"12:10",end:"12:55",type:"class",subject:"Ciencias Naturales"},
+
+{start:"12:55",end:"13:35",type:"lunch"},
+
+{start:"13:35",end:"14:20",type:"class",subject:"Ciencias Naturales"}
+
+],
+
+jueves:[
+
+{start:"08:00",end:"08:45",type:"class",subject:"Lenguaje"},
+{start:"08:45",end:"09:30",type:"class",subject:"Lenguaje"},
+{start:"09:30",end:"10:15",type:"class",subject:"Ciencias Naturales"},
+
+{start:"10:15",end:"10:40",type:"break",id:"r1"},
+
+{start:"10:40",end:"11:25",type:"class",subject:"Historia"},
+{start:"11:25",end:"12:10",type:"class",subject:"Historia"},
+
+{start:"12:10",end:"12:30",type:"break",id:"r2"},
+
+{start:"12:30",end:"13:15",type:"class",subject:"Inglés"},
+{start:"13:15",end:"14:00",type:"class",subject:"Consejo Curso"},
+
+{start:"14:00",end:"14:55",type:"lunch"},
+
+{start:"14:55",end:"15:40",type:"class",subject:"Orientación"}
+
+],
+
+viernes:[
+
+{start:"08:00",end:"08:45",type:"class",subject:"Matemática"},
+{start:"08:45",end:"09:30",type:"class",subject:"Religión/TPV"},
+{start:"09:30",end:"10:15",type:"class",subject:"Religión/TPV"},
+
+{start:"10:15",end:"10:40",type:"break",id:"r1"},
+
+{start:"10:40",end:"11:25",type:"class",subject:"I.E."},
+{start:"11:25",end:"12:10",type:"class",subject:"I.E."},
+
+{start:"12:10",end:"12:30",type:"break",id:"r2"},
+
+{start:"12:30",end:"13:15",type:"class",subject:"Lenguaje"},
+{start:"13:15",end:"14:00",type:"class",subject:"Lenguaje"},
+
+{start:"14:00",end:"14:45",type:"lunch"}
+
+]
 
 };
